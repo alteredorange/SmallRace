05f09c135164ecbc9763290df6477c9294439785
Kyle Fitzgerald <alteredorange@gmail.com>
2015-05-21 09:12:17 -0600

    First Commit

---
+++ SmallRace/Assets/Game/Networking/RestarterNew.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestarterNew : MonoBehaviour {
+
+
+	public string playerPrefabName = "Car2";
+	public Transform spawnPoint1;
+	public GameObject playerPrefab;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			//PhotonNetwork.Destroy(other.gameObject);
+			Destroy(other.gameObject);
+			Instantiate (playerPrefab, spawnPoint1.position, spawnPoint1.rotation);
+		}
+	}
+}
+
